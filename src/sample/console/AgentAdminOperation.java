a7d0240bd774335b62697ddef8a2d2c29d899298
dubrovinom <interrogatus@gmail.com>
2017-12-18 16:15:54 +0300

    Initial commit

---
+++ src/sample/console/AgentAdminOperation.java
@@ -0,0 +1,19 @@
+package sample.console;
+
+import com._1c.v8.ibis.admin.AgentAdminException;
+
+/**
+ * Administrative operation on a 1C:Enterprise server cluster
+ *
+ * @param <V> operation result type
+ */
+public interface AgentAdminOperation<V>
+{
+    /**
+     * Calls the operation
+     * 
+     * @return operation result
+     * @throws AgentAdminException in the case of errors.
+     */
+    V call();
+}
